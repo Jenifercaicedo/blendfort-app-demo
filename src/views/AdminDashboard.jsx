e3eb2629ff9bf7eccdcc3aa66f37272828f9fd04
paymore <paymore@paymores-MacBook-Air.local>
2026-03-31 11:27:26 -0500

    logo tamano

--- src/views/AdminDashboard.jsx
+++ src/views/AdminDashboard.jsx
@@ -410,7 +410,7 @@ const AdminDashboard = () => {
       <img
         src={logo}
         alt="Blendfort"
-        className="h-18 sm:h-22 md:h-32 lg:h-38 xl:h36  w-auto object-contain"
+        className="h-24 sm:h-26 md:h-30 lg:h-38 xl:h36  w-auto object-contain"
       />
     </button>
 
